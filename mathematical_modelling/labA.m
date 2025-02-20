08db83e81e8cba63e747cfc3e02a8fa4b5db8e48
t3i8m <number.38.tj@gmail.com>
2025-02-20 21:24:01 +0100

    finished the first lab

--- mathematical_modelling/labA.m
+++ mathematical_modelling/labA.m
@@ -58,4 +58,83 @@ legend('Prey (x)', 'Predators (y)');
 title('Predator-Prey Population Dynamics');
 grid on;
 
+%Solve the model that has been described earlier with one of the Matlab solvers for the same
+% parameters, initial condition and time steps.
+function dydt = lotka_volterra(t, y, e1, a1, e2, a2)
+    dydt = zeros(2,1);
+    dydt(1) = e1 * y(1) - a1 * y(1) * y(2); % dx/dt
+    dydt(2) = -e2 * y(2) + a2 * y(1) * y(2); % dy/dt
+end
 
+y0 = [50; 5]; % init conditions x0 y0
+tspan = [0 1000];
+[t, Y] = ode45(@(t,y) lotka_volterra(t, y, e1, a1, e2, a2), tspan, y0);
+figure;
+plot(t, Y(:,1), 'b', 'LineWidth', 1.5); hold on;
+plot(t, Y(:,2), 'r', 'LineWidth', 1.5);
+xlabel('Time');
+ylabel('Population');
+legend('Prey (x)', 'Predators (y)');
+title('Predator-Prey Model Solution (ode45)');
+grid on;
+
+% The Lotka-Volterra model described above involves exponential growth of the preys in absence
+% of predators. It can be made more realistic by including a term which limits the growth rate
+% of the preys depending on the population size. (See for instance the model specification on
+% p.14 of the lecture notes.) This means that an extra quadratic term is included in the first
+% differential equation. Add such a term −σ1x2 to obtain the model
+
+function [x_new, y_new] = simStepImproved(x, y, e1, a1, e2, a2, dt, sigm)
+    x_new = x+(e1*x-a1*x*y-sigm*x^2)*dt;
+    y_new = y+(-e2*y+a2*x*y)*dt;
+end
+
+i = 1;
+iterations = 10000;
+new_x = zeros(iterations, 1);
+new_y = zeros(iterations, 1);
+time = (0:iterations) * dt;
+e1 = 0.04;
+a1 = 0.0005;
+e2 = 0.3;
+a2 = 0.001;
+sigm = 0.00004;
+x_eq = 50;
+y_eq = 5;
+new_x(1) = x_eq;
+new_y(1) = y_eq;
+dt = 0.1;
+while(i<=iterations)
+    i=i+1;
+    [x_new, y_new] = simStepImproved(new_x(i-1), new_y(i-1), e1, a1, e2, a2, dt, sigm);
+    new_x(i) = x_new;
+    new_y(i) = y_new;
+end
+x_eq = (e2 / a2) * (1 - sigm * e2 / (a2 * e1));
+y_eq = e2/a2;
+figure;
+plot(time, new_x, 'b', 'LineWidth', 1.5); hold on;
+plot(time, new_y, 'r', 'LineWidth', 1.5);
+xlabel('Time');
+ylabel('Population');
+legend('Prey (x)', 'Predators (y)');
+title('Predator-Prey Population Dynamics');
+grid on;
+disp(['Eq x: ', num2str(x_eq)]);
+
+disp(['Eq y: ', num2str(y_eq)]);
+
+% Consider the following state-space system in continuous time, with input u(t) and output
+% y(t):
+% x′(t) = Ax(t) + Bu(t)
+% y(t) = Cx(t) + Du(t)
+
+A = [4,1,0; -7,-1,-1; 119,32,-8];
+B = [0; 0; -1];
+C = [227, 55, -13];
+D = -2;
+
+system1 = ss(A,B,C,D);
+H = tf(system1);
+disp('Transfer Function H(s):');
+H
